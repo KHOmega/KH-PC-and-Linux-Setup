c7192bb318514661fb8beda1ec0b36ebc22009f2
Em <93887977+KHOmega@users.noreply.github.com>
2025-01-29 15:13:16 -0600

    Update ReFinedPreReq.sh
    removed references to `home/deck` and replaced with `~` for wider Linux compatibility

--- refined_specific/ReFinedPreReq.sh
+++ refined_specific/ReFinedPreReq.sh
@@ -6,19 +6,19 @@ cd
 echo "Downloading Winetricks from KH-PC-and-Linux-Setup..."
 wget https://raw.githubusercontent.com/KHOmega/KH-PC-and-Linux-Setup/refs/heads/main/refined_specific/winetricks
 echo "Moving downloaded winetricks to Protontricks's directory..."
-cp /home/deck/winetricks /var/lib/flatpak/app/com.github.Matoking.protontricks/current/active/files/bin/
+cp ~/winetricks /var/lib/flatpak/app/com.github.Matoking.protontricks/current/active/files/bin/
 echo "Installing all Re:Fined prerequisites into 2552430..."
 flatpak run com.github.Matoking.protontricks 2552430 -q -f dotnet40 dotnet48 dotnet6 dotnet7 dotnet8 dotnet9 dotnetdesktop6 dotnetdesktop7 dotnetdesktop8 dotnetdesktop9 vcrun2022 vcrun6 xaudio29 win11
 echo "Downloading Re:Fined [Steam] from GitHub..."
 cd
 wget https://github.com/KH-ReFined/KH-ReFined/releases/download/v2.60/KH2-ReFined-STEAM-v2.60.zip
 echo "Making a backup of KINGDOM HEARTS II FINAL MIX.exe"
-cd /home/deck/.local/share/Steam/steamapps/common/KINGDOM\ HEARTS\ -HD\ 1.5+2.5\ ReMIX-
+cd ~/.local/share/Steam/steamapps/common/KINGDOM\ HEARTS\ -HD\ 1.5+2.5\ ReMIX-
 mkdir Backup\ EXE
 mv KINGDOM\ HEARTS\ II\ FINAL\ MIX.exe Backup\ EXE/KINGDOM\ HEARTS\ II\ FINAL\ MIX.exe.bak
 cd
 echo "Unzipping ReFined and placing the files into KH 1.5/2.5..."
-unzip KH2-ReFined-STEAM-v2.60.zip -d /home/deck/.local/share/Steam/steamapps/common/KINGDOM\ HEARTS\ -HD\ 1.5+2.5\ ReMIX-
+unzip KH2-ReFined-STEAM-v2.60.zip -d ~/.local/share/Steam/steamapps/common/KINGDOM\ HEARTS\ -HD\ 1.5+2.5\ ReMIX-
 echo "Deleting setup files..."
 cd
 rm KH2-ReFined-STEAM-v2.60.zip
