baf3156a24959c8cd3d4643a2a16376ddfd2bf9e
Em <93887977+KHOmega@users.noreply.github.com>
2025-02-01 14:35:43 -0600

    Add files via upload

---
+++ refined_specific/OpenKHSetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd
+mkdir ~/OpenKHSetup
+cd ~/OpenKHSetup
+wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
+wget https://download.visualstudio.microsoft.com/download/pr/a1da19dc-d781-4981-84e9-ffa0c05e00e9/46f3cd2015c27a0e93d7c102a711577e/windowsdesktop-runtime-6.0.31-win-x64.exe
+unzip openkh.zip -d ~/Documents
+mv ~/Documents/openkh ~/Documents/OpenKH
+mv ~/OpenKHSetup/windowsdesktop-runtime-6.0.31-win-x64.exe ~/Documents/Downloads
+cd
+rm -rf ~/OpenKHSetup
+exit
