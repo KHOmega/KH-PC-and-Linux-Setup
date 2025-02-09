5e9936508018d5dae57b852389aa89db1195842e
Em <93887977+KHOmega@users.noreply.github.com>
2025-02-09 09:05:20 -0600

    Update GoASetup.sh
    Updated link for most recent dotnetdesktop6

--- GoASetup.sh
+++ GoASetup.sh
@@ -5,11 +5,11 @@ mkdir ~/Documents/SeedGenerator
 cd ~/SeedSetup
 wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
 wget https://github.com/tommadness/KH2Randomizer/releases/latest/download/KH2.Randomizer.exe
-wget https://download.visualstudio.microsoft.com/download/pr/a1da19dc-d781-4981-84e9-ffa0c05e00e9/46f3cd2015c27a0e93d7c102a711577e/windowsdesktop-runtime-6.0.31-win-x64.exe
+wget https://download.visualstudio.microsoft.com/download/pr/f6b6c5dc-e02d-4738-9559-296e938dabcb/b66d365729359df8e8ea131197715076/windowsdesktop-runtime-6.0.36-win-x64.exe
 unzip openkh.zip -d ~/Documents
 mv ~/Documents/openkh ~/Documents/OpenKH
 mv ~/SeedSetup/KH2.Randomizer.exe ~/Documents/SeedGenerator
-mv ~/SeedSetup/windowsdesktop-runtime-6.0.31-win-x64.exe ~/Downloads
+mv ~/SeedSetup/windowsdesktop-runtime-6.0.36-win-x64.exe ~/Downloads
 cd
 rm -rf ~/SeedSetup
 exit
