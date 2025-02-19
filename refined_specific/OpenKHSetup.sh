59f46080ea66fb28e349212cdcf7e81d4123043e
Em <93887977+KHOmega@users.noreply.github.com>
2025-02-19 05:29:37 -0600

    Update OpenKHSetup.sh

--- refined_specific/OpenKHSetup.sh
+++ refined_specific/OpenKHSetup.sh
@@ -6,7 +6,7 @@ wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
 wget https://download.visualstudio.microsoft.com/download/pr/a1da19dc-d781-4981-84e9-ffa0c05e00e9/46f3cd2015c27a0e93d7c102a711577e/windowsdesktop-runtime-6.0.31-win-x64.exe
 unzip openkh.zip -d ~/Documents
 mv ~/Documents/openkh ~/Documents/OpenKH
-mv ~/OpenKHSetup/windowsdesktop-runtime-6.0.31-win-x64.exe ~/Documents/Downloads
+mv ~/OpenKHSetup/windowsdesktop-runtime-6.0.31-win-x64.exe ~/Downloads
 cd
 rm -rf ~/OpenKHSetup
 exit
