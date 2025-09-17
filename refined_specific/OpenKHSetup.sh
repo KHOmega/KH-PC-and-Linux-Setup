ab7f73f8e51cf59e7de2b77e67f252380d912145
Em <93887977+KHOmega@users.noreply.github.com>
2025-09-16 19:53:13 -0500

    Updated link from .NET 6 to .NET 8

--- refined_specific/OpenKHSetup.sh
+++ refined_specific/OpenKHSetup.sh
@@ -3,7 +3,7 @@ cd
 mkdir ~/OpenKHSetup
 cd ~/OpenKHSetup
 wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
-wget https://download.visualstudio.microsoft.com/download/pr/a1da19dc-d781-4981-84e9-ffa0c05e00e9/46f3cd2015c27a0e93d7c102a711577e/windowsdesktop-runtime-6.0.31-win-x64.exe
+wget https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/8.0.20/windowsdesktop-runtime-8.0.20-win-x64.exe
 unzip openkh.zip -d ~/Documents
 mv ~/Documents/openkh ~/Documents/OpenKH
 mv ~/OpenKHSetup/windowsdesktop-runtime-6.0.31-win-x64.exe ~/Downloads
