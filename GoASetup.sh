4a566b4c0206d1a97a40c1601c1e6bb9659ae014
Em <93887977+KHOmega@users.noreply.github.com>
2025-09-16 20:07:08 -0500

    Updated link from .NET 6 to .NET 8

--- GoASetup.sh
+++ GoASetup.sh
@@ -5,11 +5,11 @@ mkdir ~/Documents/SeedGenerator
 cd ~/SeedSetup
 wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
 wget https://github.com/tommadness/KH2Randomizer/releases/latest/download/KH2.Randomizer.exe
-wget https://download.visualstudio.microsoft.com/download/pr/f6b6c5dc-e02d-4738-9559-296e938dabcb/b66d365729359df8e8ea131197715076/windowsdesktop-runtime-6.0.36-win-x64.exe
+wget https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/8.0.20/windowsdesktop-runtime-8.0.20-win-x64.exe
 unzip openkh.zip -d ~/Documents
 mv ~/Documents/openkh ~/Documents/OpenKH
 mv ~/SeedSetup/KH2.Randomizer.exe ~/Documents/SeedGenerator
-mv ~/SeedSetup/windowsdesktop-runtime-6.0.36-win-x64.exe ~/Downloads
+mv ~/SeedSetup/windowsdesktop-runtime-8.0.20-win-x64.exee ~/Downloads
 cd
 rm -rf ~/SeedSetup
 exit
