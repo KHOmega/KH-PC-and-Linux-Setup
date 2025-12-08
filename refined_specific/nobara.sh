4cfd9f8f1977b2e9189ad9697259e2d8364d6d57
Em <93887977+KHOmega@users.noreply.github.com>
2025-12-08 00:06:30 -0600

    Add files via upload

---
+++ refined_specific/nobara.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Re:Fined Dependency Installer (Fedora) by KHOmega"
+sleep 3
+echo "During this setup, a version of Winetricks will replace the one included within Protontricks taken from an install of Heroic Games Launcher. This is because it has the necessary dependencies needed to run Re:Fined and Panacea on Fedora."
+sleep 3
+echo "If you do not have a password set, please exit this script and setup a password for Konsole by using passwd!"
+sleep 3
+echo "Starting the script now!"
+sleep 3
+echo "Attempting to get permission to access Protontrick's directory, you will need to type in your password!"
+sleep 3
+sudo chmod -R 777 ~/.local/share/flatpak/app/com.github.Matoking.protontricks/current/active/files/bin/
+cd ~
+echo "Creating temporary directory..."
+mkdir refined_temp
+cd refined_temp
+echo "Downloading Winetricks from KH-PC-and-Linux-Setup..."
+sleep 3
+wget https://raw.githubusercontent.com/KHOmega/KH-PC-and-Linux-Setup/refs/heads/main/refined_specific/winetricks
+echo "Moving downloaded winetricks to Protontricks's directory..."
+sleep 3
+cp ~/refined_temp/winetricks ~/.local/share/flatpak/app/com.github.Matoking.protontricks/current/active/files/bin/
+echo "Installing dependencies into 2552430..."
+sleep 3
+flatpak run com.github.Matoking.protontricks 2552430 -q -f dotnet8 dotnetdesktop8 vcrun6 ucrtbase2019 xaudio29 win11
+sleep 3
+cd ~/refined_temp
+sleep 3
+echo "Cleaning up setup files..."
+sleep 3
+cd
+rm -r refined_temp
+echo "All complete!"
