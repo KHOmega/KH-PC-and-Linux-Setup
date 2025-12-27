d28548efe0ff5765dd2405bfe5e2ee137faa0eba
Em <93887977+KHOmega@users.noreply.github.com>
2025-12-26 21:04:54 -0600

    Refine dependency setup script for better execution
    Script refined by itslaguna to work better on more distros and mitigate less errors on dll override

--- refined_specific/dependencysetup.sh
+++ refined_specific/dependencysetup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo "Re:Fined Dependency Installer by KHOmega"
 sleep 3
-cd ~
-sleep 3
-cd Desktop
+cd "$HOME"
 wget https://raw.githubusercontent.com/KHOmega/KH-PC-and-Linux-Setup/refs/heads/main/refined_specific/add_registry.bat
 echo "Now installing..."
 sleep 3
 flatpak run com.github.Matoking.protontricks 2552430 -q -f dotnet8 dotnetdesktop8 vcrun6 ucrtbase2019 xaudio29
-flatpak run --command=protontricks-launch com.github.Matoking.protontricks --appid 2552430 /home/$USER/Desktop/add_registry.bat
+flatpak run --filesystem="$HOME" --command=protontricks-launch com.github.Matoking.protontricks --appid 2552430 "$HOME/add_registry.bat"
 sleep 3
 echo "Cleaning up setup files..."
 sleep 3
-rm add_registry.bat
+rm "add_registry.bat"
 echo "Complete. You may now close this window."
