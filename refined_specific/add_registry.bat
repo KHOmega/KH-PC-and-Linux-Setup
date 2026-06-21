02d4e411b18c041bdb7fefe8632801e713340655
Em <93887977+KHOmega@users.noreply.github.com>
2026-06-20 20:28:24 -0500

    Add additional registry entries for Wine DllOverrides
    Added `dinput8`, and `LuaBackend`

--- refined_specific/add_registry.bat
+++ refined_specific/add_registry.bat
@@ -1,2 +1,4 @@
 @echo off
 REG ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "version" /t REG_SZ /d "native,builtin" /f
+REG ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "LuaBackend" /t REG_SZ /d "native,builtin" /f
+REG ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "dinput8" /t REG_SZ /d "native,builtin" /f
