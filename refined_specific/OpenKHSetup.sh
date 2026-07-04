2957d1eb3fbd8075ef4a1ca85948f9b3bbe5a2f9
Em <93887977+KHOmega@users.noreply.github.com>
2026-07-04 14:07:51 -0500

    Enhance OpenKHSetup.sh for setup and downloads
    Added commands to create OpenKH setup folder and download necessary files.

--- refined_specific/OpenKHSetup.sh
+++ refined_specific/OpenKHSetup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# This three commands makes sure you're in your root folder and will make a dedicated OpenKH Setup Folder
 cd
 mkdir ~/OpenKHSetup
 cd ~/OpenKHSetup
+# The following two commands will download the latest OpenKH download and .NET 8.0.26 into that new folder.
 wget https://github.com/OpenKH/OpenKh/releases/download/latest/openkh.zip
 wget https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/8.0.26/windowsdesktop-runtime-8.0.26-win-x64.exe
+# This will then unzip OpenKH and place it into your Documents folder, and delete the unzipped folder.
 unzip openkh.zip -d ~/Documents
-mv ~/OpenKHSetup/windowsdesktop-runtime-8.0.26-win-x64.exe ~/Downloads
-cd
-rm -rf ~/OpenKHSetup
+rm -r -f openkh.zip
 exit
